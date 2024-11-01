45574e7940fb3af227bd5b169331ddc5ae0a5579
Marcelo Frohlich <96299094+MarceloFrohlich@users.noreply.github.com>
2024-11-01 10:40:48 -0300

    01/11

---
+++ delicias_miuda/src/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <section>
+      <div className="md:h-[450px] lg:h-[600px] h-[300px] bg-web-main-banner bg-cover bg-no-repeat bg-center">
+        <h1>teste</h1>
+      </div>
+    </section>
+  );
+}
